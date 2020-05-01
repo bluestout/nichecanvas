22637b6d4c34f6e676dd85e1d5a5da80728428b4
FirstSuns <sun.first2016@yandex.com>
2020-05-01 14:10:19 -0700

    add zoom on prouct page

---
+++ assets/zendesk-embeddable.js
@@ -0,0 +1,1 @@
+var script = document.createElement('script');script.type = 'text/javascript';script.id = 'ze-snippet';script.src = 'https://static.zdassets.com/ekr/snippet.js?key=e7412443-5e17-4c51-8709-60170c1a6338';document.getElementsByTagName('head')[0].appendChild(script);
